9f9419313e5a7f09d622693fc2d77099df276d30
yoshiki <interestingyoshiki@gmail.com>
2018-03-04 00:12:21 +0900

    Add findAllByFeedIdメソッドの追加

--- src/main/kotlin/com/github/ikatyan/rssreader/controller/RssReaderController.kt
+++ src/main/kotlin/com/github/ikatyan/rssreader/controller/RssReaderController.kt
@@ -1,7 +1,9 @@
 package com.github.ikatyan.rssreader.controller
 
+import com.github.ikatyan.rssreader.entity.Article
 import com.github.ikatyan.rssreader.service.article.ArticleService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,8 +13,12 @@ class HelloController(private val articleService: ArticleService) {
 
     @GetMapping("/")
     fun index(): String {
-
-        val article = articleService.findOneByArticleId(1) ?: return "Not Found"
-        return article.title
+        return "Hello World!"
     }
+
+    @GetMapping("/article/{feedId}")
+    fun findArticlesByFeedId(@PathVariable(value = "feedId") feedId: Int): List<Article>? {
+        return articleService.findAllByFeedId(feedId)
+    }
+
 }
