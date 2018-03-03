553a1647aa2c81e76aa471ffed5333e4601cf90d
yoshiki <interestingyoshiki@gmail.com>
2018-03-04 00:11:39 +0900

    Add findAllByFeedIdメソッドの追加

--- src/main/kotlin/com/github/ikatyan/rssreader/repository/ArticleRepository.kt
+++ src/main/kotlin/com/github/ikatyan/rssreader/repository/ArticleRepository.kt
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface ArticleRepository : JpaRepository<Article, Int> {
     fun existsByLink(link: String): Boolean
+    fun findAllByFeedId(feedId: Int): List<Article>
 }
 
 interface FeedRepository : JpaRepository<Feed, Int>
