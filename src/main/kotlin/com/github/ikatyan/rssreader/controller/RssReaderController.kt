877600a74b48977d639ec5efe2eb4e4cc8b1f972
yoshiki <interestingyoshiki@gmail.com>
2018-03-04 20:00:54 +0900

    Add findAllFeedsメソッド追加

--- src/main/kotlin/com/github/ikatyan/rssreader/controller/RssReaderController.kt
+++ src/main/kotlin/com/github/ikatyan/rssreader/controller/RssReaderController.kt
@@ -1,7 +1,9 @@
 package com.github.ikatyan.rssreader.controller
 
 import com.github.ikatyan.rssreader.entity.Article
+import com.github.ikatyan.rssreader.entity.Feed
 import com.github.ikatyan.rssreader.service.article.ArticleService
+import com.github.ikatyan.rssreader.service.feed.FeedService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
-class HelloController(private val articleService: ArticleService) {
+class HelloController(
+        private val articleService: ArticleService,
+        private val feedService: FeedService) {
 
     @GetMapping("/")
     fun index(): String {
@@ -19,6 +23,11 @@ class HelloController(private val articleService: ArticleService) {
     @GetMapping("/article/{feedId}")
     fun findArticlesByFeedId(@PathVariable(value = "feedId") feedId: Int): List<Article>? {
         return articleService.findAllByFeedId(feedId)
+    }
+
+    @GetMapping("/feed")
+    fun findAllFeeds(): List<Feed>? {
+        return feedService.findAll()
     }
 
 }
