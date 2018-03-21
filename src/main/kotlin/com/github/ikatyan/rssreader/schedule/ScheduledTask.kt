c917252acac45fe32c615abc485bc0f080460c48
yoshiki <interestingyoshiki@gmail.com>
2018-03-21 20:45:40 +0900

    Update printlnメソッドをlogger.infoに変更

--- src/main/kotlin/com/github/ikatyan/rssreader/schedule/ScheduledTask.kt
+++ src/main/kotlin/com/github/ikatyan/rssreader/schedule/ScheduledTask.kt
@@ -3,6 +3,7 @@ package com.github.ikatyan.rssreader.schedule
 import com.github.ikatyan.rssreader.repository.ArticleRepository
 import com.github.ikatyan.rssreader.repository.FeedRepository
 import com.github.ikatyan.rssreader.service.rss.CheckFeedService
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -10,14 +11,16 @@ import org.springframework.stereotype.Component
 class ScheduledTask(
         private val checkFeedService: CheckFeedService,
         private val articleRepository: ArticleRepository,
-        private val feedRepository: FeedRepository) {
+        private val feedRepository: FeedRepository
+) {
+    private val logger = LoggerFactory.getLogger(ScheduledTask::class.java)
 
-    @Scheduled(cron = "* */30 * * * *")
+    @Scheduled(fixedDelay = 30 * 60 * 1000)
     fun findNewArticles() {
-        println("Scheduled Task Start")
+        logger.info("Scheduled Task Start")
         val feedList = feedRepository.findAll() ?: return
         checkFeedService.fetchNewArticles(feedList)
                 .forEach { articleRepository.save(it) }
-        println("Scheduled Task End")
+        logger.info("Scheduled Task End")
     }
 }
